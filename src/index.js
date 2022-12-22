13d478b245723fa9d4ad06fd0cc6fd311dc68e81
Ковалёв Александр Викторович <AKovalev4@alfabank.ru>
2022-12-22 10:21:47 +0300

    feat(index.js): change export

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-const visitor = {
-    UnaryExpression(path) {
-        if (path.parent.type === 'MemberExpression' && path.parent.object.type === 'Identifier' && path.node.argument.type === 'NumericLiteral' && Number.isInteger(path.node.argument.value)) {
-            path.replaceWithSourceString(`${path.parent.object.name}.length - ${path.node.argument.value}`);
-        }
-    },
-};
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 
-module.exports = () => {
+exports.default = function (_ref) {
     return {
-        visitor
+        visitor: {
+            UnaryExpression(path) {
+                if (path.parent.type === 'MemberExpression' && path.parent.object.type === 'Identifier' && path.node.argument.type === 'NumericLiteral' && Number.isInteger(path.node.argument.value)) {
+                    path.replaceWithSourceString(`${path.parent.object.name}.length - ${path.node.argument.value}`);
+                }
+            }
+        }
     }
 }
